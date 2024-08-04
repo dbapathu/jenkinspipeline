6d550cb34de908c87846e285f367208ae94a9525
dbapathu <75102888+dbapathu@users.noreply.github.com>
2024-08-03 22:27:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "jenkinstfansible" {
   ami           = "ami-00b1dcc8d3eab4fdb"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "jenkinstfansible"
