e9117546882d1afa3ee71c185c7857c15e169f11
dbapathu <75102888+dbapathu@users.noreply.github.com>
2024-08-03 22:14:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key= "AKIA5PAC2RKBSRZ5TQ5J"
-  secret_key= "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"
 }
 
 resource "aws_instance" "jenkinstfansible" {
