d0d5bf64416e9cf5770898c401e8e002246fa236
dbapathu <75102888+dbapathu@users.noreply.github.com>
2024-08-04 20:17:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "jenkinstfansible" {
   ami           = "ami-0875345809c54b79e"
   instance_type = "t2.micro"
-  key_name = TF_key
+  key_name = "TF_key"
 
   tags = {
     Name = "jenkinstfansibleec2"
